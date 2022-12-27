12b05eab1d6388003fcb2ac12fbb897552f7f72f
Fernando Hasperue <fernando.hasperue@gmail.com>
2022-12-27 09:56:18 -0300

    agregando efectos

--- animaciones.js
+++ animaciones.js
@@ -19,4 +19,24 @@ window.addEventListener('scroll', function() {
     });
   });
   
- 
+
+  let i = 0;
+let text = ">Desarrollador web Fullstack ";
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100);
+  } else {
+    // Crea un intervalo de parpadeo cada 500 milisegundos
+    setInterval(function() {
+      document.getElementById("text").innerHTML = text.substring(0, text.length - 1) + " _";
+      setTimeout(function() {
+        document.getElementById("text").innerHTML = text;
+      }, 250);
+    }, 500);
+  }
+}
+
+typeWriter();
