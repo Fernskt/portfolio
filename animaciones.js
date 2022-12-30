4efc91fe85af162a71f1eee88ec8b2d0c7f0c377
Fernando Hasperue <fernando.hasperue@gmail.com>
2022-12-30 12:21:36 -0300

    agregando skills

--- animaciones.js
+++ animaciones.js
@@ -1,24 +1,33 @@
  
-window.addEventListener('scroll', function() {
-    // Obtener todos los elementos con la clase ".mi-seccion"
-    var elements = document.querySelectorAll('.mi-seccion');
-  
-    // Recorrer la lista de elementos
-    elements.forEach(function(element) {
-      // Obtener la posición del elemento en la pantalla
-      var elementPosition = element.getBoundingClientRect().top;
-  
-      // Obtener la altura de la pantalla
-      var screenHeight = window.innerHeight;
-  
-      // Comprobar si el elemento está en el área visible de la pantalla
-      if (elementPosition < screenHeight) {
-        // Añadir la clase "active" al elemento para iniciar la animación
-        element.classList.add('active');
-      }
-    });
+function animateOnScroll(selector, animationClass) {
+  // Obtener todos los elementos con la clase especificada en el selector
+  var elements = document.querySelectorAll(selector);
+
+  // Recorrer la lista de elementos
+  elements.forEach(function(element) {
+    // Obtener la posición del elemento en la pantalla
+    var elementPosition = element.getBoundingClientRect().top;
+
+    // Obtener la altura de la pantalla
+    var screenHeight = window.innerHeight;
+
+    // Comprobar si el elemento está en el área visible de la pantalla
+    if (elementPosition < screenHeight) {
+      // Añadir la clase especificada en la variable animationClass al elemento para iniciar la animación
+      element.classList.add(animationClass)
+    }
   });
-  
+}
+
+window.addEventListener('scroll', function() {
+  animateOnScroll('.mi-seccion', 'active');
+});
+
+window.addEventListener('scroll', function() {
+  animateOnScroll('.progress-circle', 'animation');
+});
+
+/*Animacion Maquina de escribir y linea de comando*/
 
   let i = 0;
 let text = ">Desarrollador web Fullstack ";
